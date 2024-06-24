f0601015ae745a862acaa868bbc59a5ca369d57d
dgbonett <88499135+dgbonett@users.noreply.github.com>
2024-06-23 20:02:46 -0700

    Update meta_model.R

--- R/meta_model.R
+++ R/meta_model.R
@@ -790,7 +790,8 @@ meta.lm.spear <- function(alpha, n, cor, X) {
 #' @param     alpha	 alpha level for 1-alpha confidence
 #' @param     n      vector of sample sizes
 #' @param     cor		 vector of estimated semipartial correlations
-#' @param     r2     vector of estimated squared multiple correlations for full model
+#' @param     r2   	  vector of squared multiple correlations for a model that
+#' includes the IV and all control variables
 #' @param     X		   matrix of predictor values
 #' 
 #' 
